490850bdd21629216d8c48ab3b3cab8265fa319c
Sherina Buenaseda <sherina76@gmail.com>
2023-07-24 18:55:43 -0600

    Completed JavaScript to Python conversion Functions Lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default()
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    result = num1 + num2
+    print (result)
+    # return result
+num1 = 1
+num2 = 2
+add(num1,num2)
+
 
 def halve(number):
-    pass
+    result = number / 2
+    print(result)
+number = 4
+halve(4)
+
+
+
+
